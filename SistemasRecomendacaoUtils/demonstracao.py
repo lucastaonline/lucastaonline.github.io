9f7737fd64c03968687c74d58bedb3daa8919efd
Binario <lucas.pereira@automind.com.br>
2022-04-02 19:36:13 -0300

    Ajustes

---
+++ SistemasRecomendacaoUtils/demonstracao.py
@@ -0,0 +1,6 @@
+esse_workshop_eh_muito_bom = True
+
+if esse_workshop_eh_muito_bom:
+    print('Nota máxima pra esses alunos aí hein?')
+else:
+    print('Tava no celular e não prestou atenção né...')
